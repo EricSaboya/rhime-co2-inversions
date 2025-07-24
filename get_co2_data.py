ef1730e492dbd51aa6c2d47cf2dce4ee36d9c5d8
Alice Ramsden <alice.ramsden@metoffice.gov.uk>
2025-07-24 10:19:59 +0100

    merge fix again, to align with main

--- get_co2_data.py
+++ get_co2_data.py
@@ -408,7 +408,6 @@ def get_mf_obs_sims(flux_dict: dict,
                                                                flux=data_dict[".flux"],
                                                                bc=data_dict[".bc"],
                                                               )
-                
                 split_by_sector = len(flux_dict["source"]) > 1
                 scenario_combined = model_scenario.footprints_data_merge(calc_fp_x_flux=True, split_by_sector=split_by_sector)
 
@@ -421,6 +420,7 @@ def get_mf_obs_sims(flux_dict: dict,
                         scenario_combined[f"Hall_{s}"] = scenario_combined.fp_x_flux_sectoral.sel(source=s, drop=True)
 
                     scenario_combined = scenario_combined.drop_vars([mf_mod_var, "fp_x_flux_sectoral"])
+                    
                 data_dict[site] = scenario_combined
                 # data_dict[site].bc_mod.values *= 1e-3 # convert from ppb (default in openghg) to ppm
                 data_dict[site].bc_mod.values *= 1e-9 # convert from ppb (default in openghg) to mol/mol
